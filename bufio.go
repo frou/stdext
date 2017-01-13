baa09f696b4d0a2976ffec6709cb668fdcf05545
Duncan Holm <duncan.holm@moray.gov.uk>
2017-01-13 11:14:51 +0000

    Add func ForEachLine

--- bufio.go
+++ bufio.go
@@ -3,8 +3,19 @@ package stdext
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 )
+
+// ForEachLine reads lines from r and applies f to each of them. The string
+// arguments to f will not include line-endings.
+func ForEachLine(r io.Reader, f func(string)) error {
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		f(scanner.Text())
+	}
+	return scanner.Err()
+}
 
 // PressReturnTo prints a message with the given verb to stderr, then returns
 // after reading a line from stdin (which is discarded).
