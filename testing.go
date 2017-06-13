24331cfea392af10157b719f57cf91062b98ab43
Duncan Holm <duncan.holm@moray.gov.uk>
2017-06-13 13:22:34 +0100

    Comment

--- testing.go
+++ testing.go
@@ -5,13 +5,17 @@ import (
 	"testing/quick"
 )
 
+// QuickCheckStrings tests that the function pred holds true when bombarded by
+// randomly generated strings.
 func QuickCheckStrings(t *testing.T, pred func(string) bool) {
 	QuickCheckStringsN(t, 0 /*Use Default*/, pred)
 }
 
-func QuickCheckStringsN(t *testing.T, count int, pred func(string) bool) {
+// QuickCheckStringsN tests that the function pred holds true when bombarded by
+// n randomly generated strings.
+func QuickCheckStringsN(t *testing.T, n int, pred func(string) bool) {
 	cfg := quick.Config{
-		MaxCount: count,
+		MaxCount: n,
 	}
 	if err := quick.Check(pred, &cfg); err != nil {
 		t.Error(err)
