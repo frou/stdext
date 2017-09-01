c47e04acbaa32291bf2a9697f73730739d554911
Duncan Holm <duncan.holm@moray.gov.uk>
2017-09-01 14:06:22 +0100

    Add CompareAndSwap method

--- sync.go
+++ sync.go
@@ -23,11 +23,29 @@ func (a *ConcAtom) Deref() interface{} {
 	return a.val
 }
 
+// TODO(DH): Rename to Swap?
 func (a *ConcAtom) Replace(val interface{}) {
 	a.mu.Lock()
 	defer a.mu.Unlock()
 
 	a.val = val
+}
+
+func (a *ConcAtom) CompareAndSwap(expected, val interface{}) bool {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+
+	defer func() {
+		// It's possible that the == operator panicked with two interface value
+		// operands: https://golang.org/ref/spec#Comparison_operators
+		recover()
+	}()
+
+	if a.val == expected {
+		a.val = val
+		return true
+	}
+	return false
 }
 
 func (a *ConcAtom) Advance(old2new func(interface{}) interface{}) {
@@ -36,5 +54,3 @@ func (a *ConcAtom) Advance(old2new func(interface{}) interface{}) {
 
 	a.val = old2new(a.val)
 }
-
-// TODO(DH): CompareAndSwap method
